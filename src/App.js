d4eb97baa8e16d666cd791a901c4e51115726c50
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-08-18 11:38:39 +0300

    React router installed and imported

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.scss";
+import { BrowserRouter, Switch, Route } from "react-router";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
