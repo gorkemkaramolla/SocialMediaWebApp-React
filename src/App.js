4782d9f6e17fa0a71eb9312225cdf8ea1bba8c27
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-02 17:54:49 +0300

    Route added for Register page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React from "react";
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Register from "./components/Auth/Register";
 import Writer from "./components/Writer/Writer";
 import NavBarComponent from "./components/Navbar/NavBarComponent";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <NavBarComponent></NavBarComponent>
-      <Routes>
-        <Route exact path="/cards" element={<Home />}></Route>
-        <Route exact path="/writers/:writerId" element={<Writer />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <NavBarComponent></NavBarComponent>
+            <Routes>
+                <Route exact path="/cards" element={<Register />}></Route>
+                <Route
+                    exact
+                    path="/writers/:writerId"
+                    element={<Writer />}
+                ></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
