5eaf260af45eacded27def49102e683ccdbb2ed4
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-04 22:38:06 +0300

    Registration form implemented

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,44 +1,122 @@
 import React from "react";
 import { TextField } from "@mui/material";
-import Button from "react-bootstrap/Button";
+import { GiTripleYin } from "react-icons/gi";
 import LoginIcon from "@mui/icons-material/Login";
 import IconButton from "@mui/material/IconButton";
-import { calculateNewValue } from "@testing-library/user-event/dist/utils";
+import { styled } from "@mui/material/styles";
+import LoadingButton from "@mui/lab/LoadingButton";
+import SaveIcon from "@mui/icons-material/Save";
+import SendIcon from "@mui/icons-material/Send";
+import { red, orange } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import RegisterError from "../Errors/RegisterError";
+import "./Register.scss";
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function Register() {
+    const [loading, setLoading] = React.useState(false);
+    const [registerError, setRegisterError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    useEffect(() => {}, [registerError.error]);
+    function handleClick() {
+        setLoading(true);
+        handleRegisterRequest();
+    }
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleEmail = (value) => {
+        setEmail(value);
+    };
+    const handlePassword = (value) => {
+        setPassword(value);
+    };
+
+    const handleRegisterRequest = () => {
+        if (email)
+            axios
+                .post("/auth/register", {
+                    email: email,
+                    password: password,
+                })
+                .then((response) => {
+                    console.log(response.data);
+                })
+                .catch((error) => {
+                    setRegisterError(true);
+                    setErrorMessage(error.response.data);
+
+                    setLoading(false);
+                });
+    };
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "rgba(199,128, 137, 1)",
+            },
+        },
+    });
     return (
         <div
             style={{ minHeight: "calc(100vh - 80px)" }}
             className="d-flex flex-column  justify-content-center align-items-center"
         >
+            <div className="" style={{ fontFamily: "Brush Script MT" }}>
+                <h1>Register</h1>
+            </div>
             <div
-                className="container   "
+                className="container"
                 style={{
-                    background: "rgb(132, 88, 179,0.1)",
-                    padding: "300px 100px",
+                    padding: "100px 100px",
                     maxWidth: "800px",
                 }}
             >
                 <div className="row">
-                    <div className="col-lg-6">
+                    <div className=" ">
                         <div className="d-flex flex-column   ">
                             <TextField
+                                value={email}
+                                onChange={(mail) => {
+                                    handleEmail(mail.target.value);
+                                }}
                                 className="mb-3 "
-                                label="email"
+                                label="Email"
                             ></TextField>
-
-                            <TextField label="password"></TextField>
+                            <TextField
+                                value={password}
+                                onChange={(pw) => {
+                                    handlePassword(pw.target.value);
+                                }}
+                                className="mb-3 "
+                                label="Password"
+                                type="password"
+                            ></TextField>
+                            {registerError && (
+                                <RegisterError
+                                    errorMessage={errorMessage}
+                                ></RegisterError>
+                            )}
                         </div>
-                    </div>
-                    <div className="col-lg-6 align-self-center">
-                        <div className="d-flex flex-column  ">
-                            <h4>GO TO LOGIN </h4>
-
-                            <button className="btn btn-outline-dark mr-md-3 mb-2 mb-md-0">
-                                <IconButton color="inherit">
-                                    <LoginIcon />
-                                </IconButton>
-                            </button>
+                        <div className=" align-self-center ">
+                            <div className="d-flex flex-column  ">
+                                <ThemeProvider theme={theme}>
+                                    <LoadingButton
+                                        sx={{
+                                            minHeight: 56,
+                                        }}
+                                        onClick={handleClick}
+                                        loading={loading}
+                                        loadingPosition="end"
+                                        endIcon={<SendIcon />}
+                                        variant="contained"
+                                        color="primary"
+                                    >
+                                        Submit
+                                    </LoadingButton>
+                                </ThemeProvider>
+                            </div>
                         </div>
                     </div>
                 </div>
