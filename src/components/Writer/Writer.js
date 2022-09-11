46d2eb2966ad2575650f1648a91fdca7bb841a7d
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-11 13:52:49 +0300

    Writer Profile Page

--- src/components/Writer/Writer.js
+++ src/components/Writer/Writer.js
@@ -1,6 +1,102 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 export default function Writer() {
+    const [writerInfo, setWriterInfo] = useState({
+        userName: "",
+        bio: "",
+        email: "",
+    });
+    let axiosConfig = {
+        headers: {
+            Authorization:
+                "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjYyNzI0MzMzLCJleHAiOjE2NjMwNjY2NzV9.fk21fVMuydrPOgD1wdLtxV8DNARR6FsrK5_rVaacMYzr_VpA8EkjUfe4U292e3kHzX1LOVBJBZgGLOUc_DEgaQ",
+        },
+    };
+    useEffect(() => {
+        getWriterInformations();
+    }, []);
+    const getWriterInformations = () => {
+        axios
+            .get("/writers/" + writerId, axiosConfig)
+            .then((response) => {
+                console.log(response.data);
+                setWriterInfo((prev) => ({
+                    ...prev,
+                    userName: response.data.userName,
+
+                    bio: response.data.bio,
+                    email: response.data.email,
+                }));
+            })
+            .catch((e) => {
+                console.error(e);
+            });
+    };
+    const inputBio = (value) => {
+        setWriterInfo((prev) => ({
+            ...prev,
+            bio: value,
+        }));
+    };
+    const inputUsername = (value) => {
+        setWriterInfo((prev) => ({
+            ...prev,
+            userName: value,
+        }));
+    };
+    const inputEmail = (value) => {
+        setWriterInfo((prev) => ({
+            ...prev,
+            email: value,
+        }));
+    };
     const { writerId } = useParams();
-    return <div>Writer id : {writerId}</div>;
+    return (
+        <div>
+            <h1>{writerInfo.email}</h1>
+            <h1>{writerInfo.bio}</h1>
+            <h1>{writerInfo.userName}</h1>
+            <div>
+                <div>
+                    <label style={{ width: "80px" }}>email</label>
+
+                    <input
+                        onChange={inputEmail}
+                        value={writerInfo.email}
+                        style={{ width: "300px" }}
+                        type="text"
+                        id="email"
+                        defaultValue={writerInfo.email}
+                    />
+                    <button>change</button>
+                </div>
+                <div>
+                    <label style={{ width: "80px" }}>bio</label>
+
+                    <input
+                        onChange={inputBio}
+                        value={writerInfo.bio}
+                        style={{ width: "300px" }}
+                        type="text"
+                        id="bio"
+                        defaultValue={writerInfo.bio}
+                    />
+                    <button>change</button>
+                </div>
+                <div>
+                    <label style={{ width: "80px" }}>username</label>
+                    <input
+                        onChange={inputUsername}
+                        value={writerInfo.userName}
+                        style={{ width: "300px" }}
+                        type="text"
+                        id="userName"
+                        defaultValue={writerInfo.userName}
+                    />
+                    <button onClick={}>change</button>
+                </div>
+            </div>
+        </div>
+    );
 }
