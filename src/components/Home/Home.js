b2a869f253dd20fca640bd3399ea83349b83a52c
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-12 14:07:39 +0300

    file Changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,79 +8,78 @@ import { homeReducer, INITIAL_STATE } from "./homeReducers/homeReducer";
 import { ACTION_TYPES } from "./homeReducers/homeActionTypes";
 
 export default function Home() {
-  const [state, dispatch] = useReducer(homeReducer, INITIAL_STATE);
-  let axiosConfig = {
-    headers: {
-      Authorization: localStorage.getItem("access"),
-    },
-  };
-  const refreshPost = async () => {
-    await axios
-      .get("/posts", axiosConfig)
-      .then((response) => {
-        dispatch({
-          type: ACTION_TYPES.success,
-          payload: response.data,
-        });
-        console.log(response.data);
-      })
-      .catch((error) => {
-        dispatch({ type: ACTION_TYPES.error });
+    const [state, dispatch] = useReducer(homeReducer, INITIAL_STATE);
+    let axiosConfig = {
+        headers: {
+            Authorization: localStorage.getItem("access"),
+        },
+    };
+    const refreshPost = async () => {
+        await axios
+            .get("/posts", axiosConfig)
+            .then((response) => {
+                dispatch({
+                    type: ACTION_TYPES.success,
+                    payload: response.data,
+                });
+                console.log(response.data);
+            })
+            .catch((error) => {
+                dispatch({ type: ACTION_TYPES.error });
 
-        console.log(error);
-      });
-  };
+                console.log(error);
+            });
+    };
 
-  useEffect(() => {
-    refreshPost();
-  }, []);
+    useEffect(() => {
+        refreshPost();
+    }, []);
 
-  if (state.error) {
-    return <div>Error</div>;
-  } else if (!state.loading) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div className="container">
-        <div
-          className="row d-flex justify-content-center home-bg"
-          style={{
-            minWidth: "360px",
-            padding: "30px",
-            borderRadius: "20px",
-          }}
-        >
-          <div className="d-flex flex-column justify-content-center ">
-            <div
-              style={{ minWidth: "350px" }}
-              className="col-lg-6 col-md-6 col-xs-6 col-sm-6 col-xxl-6 col-6 col-xs-fluid align-self-center"
-            >
-              <PostForm
-                name="Görkem"
-                lastName="karamolla"
-                writerId={1}
-                refreshPost={refreshPost}
-              ></PostForm>
+    if (state.error) {
+        return <div>Error</div>;
+    } else if (!state.loading) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className="container">
+                <div
+                    className="row d-flex justify-content-center home-bg"
+                    style={{
+                        minWidth: "360px",
+                        padding: "30px",
+                        borderRadius: "20px",
+                    }}
+                >
+                    <div className="d-flex flex-column justify-content-center ">
+                        <div
+                            style={{ minWidth: "350px" }}
+                            className="col-lg-6 col-md-6 col-xs-6 col-sm-6 col-xxl-6 col-6 col-xs-fluid align-self-center"
+                        >
+                            <PostForm
+                                name="Görkem"
+                                lastName="karamolla"
+                                writerId={1}
+                                refreshPost={refreshPost}
+                            ></PostForm>
+                        </div>
+                        {state.postList.map((element) => (
+                            <div
+                                style={{ minWidth: "350px" }}
+                                className="col-lg-6 col-md-6 col-xs-6 col-sm-6 col-xxl-6 col-6 align-self-center  "
+                            >
+                                <PostCard
+                                    postId={element.id}
+                                    writerId={element.userId}
+                                    userName={element.userName}
+                                    title={element.title}
+                                    content={element.content}
+                                    postLikes={element.postLikes}
+                                ></PostCard>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
-            {state.postList.map((element) => (
-              <div
-                style={{ minWidth: "350px" }}
-                className="col-lg-6 col-md-6 col-xs-6 col-sm-6 col-xxl-6 col-6 align-self-center  "
-              >
-                <PostCard
-                  postId={element.id}
-                  writerId={element.userId}
-                  lastName={element.lastName}
-                  name={element.name}
-                  title={element.title}
-                  content={element.content}
-                  postLikes={element.postLikes}
-                ></PostCard>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
--- src/components/PostComponents/PostCard.js
+++ src/components/PostComponents/PostCard.js
@@ -21,203 +21,207 @@ import { ACTION_TYPES } from "./PostReducers/postActionTypes";
 import { Link } from "react-router-dom";
 
 const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+    }),
 }));
 
 export default function PostCard(props) {
-  const [expanded, setExpanded] = useState(false);
-  const [liked, setLiked] = useState(false);
-  const isMounted = useRef(false);
+    const [expanded, setExpanded] = useState(false);
+    const [liked, setLiked] = useState(false);
+    const isMounted = useRef(false);
 
-  const { postLikes, writerId, postId, lastName, name, content, title } = props;
-  const [refreshComments, setRefreshComments] = useState(true);
+    const { postLikes, writerId, postId, userName, content, title } = props;
+    const [refreshComments, setRefreshComments] = useState(true);
 
-  const [state, dispatch] = useReducer(postReducer, INITIAL_STATE);
-  const [likeCount, setLikeCount] = useState(postLikes.length);
-  const [likeId, setLikeId] = useState();
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-    if (!expanded) getComments();
-  };
+    const [state, dispatch] = useReducer(postReducer, INITIAL_STATE);
+    const [likeCount, setLikeCount] = useState(postLikes.length);
+    const [likeId, setLikeId] = useState();
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+        if (!expanded) getComments();
+    };
 
-  const likedOrNot = () => {
-    const likeControl = postLikes.find((like) => {
-      return like.userId === writerId;
-    });
-
-    if (likeControl != null) {
-      setLikeId(likeControl.id);
-      setLiked(true);
-    } else {
-      setLiked(false);
-    }
-  };
-  useEffect(() => {
-    likedOrNot();
-  }, []);
-
-  const handleFavorite = () => {
-    setLiked(!liked);
-    if (liked) {
-      setLikeCount(likeCount - 1);
-      deleteLike();
-    } else {
-      setLikeCount(likeCount + 1);
-      sendLike();
-    }
-  };
-  const setCommentRefresh = () => {
-    setRefreshComments(true);
-  };
-  let axiosConfig = {
-    headers: {
-      Authorization: localStorage.getItem("access"),
-    },
-  };
-  const deleteLike = () => {
-    console.log(likeId);
-    axios
-      .delete("/postlikes/" + likeId, axiosConfig)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  const sendLike = () => {
-    axios
-      .post(
-        "/postlikes",
-        {
-          postId: postId,
-          writerId: writerId,
-        },
-        axiosConfig
-      )
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  const getComments = useCallback(() => {
-    const url = "http://localhost:8080";
-    axios
-      .get(url + "/comments?postId=" + postId, axiosConfig)
-      .then((response) => {
-        dispatch({
-          type: ACTION_TYPES.success,
-          payload: response.data,
+    const likedOrNot = () => {
+        const likeControl = postLikes.find((like) => {
+            return like.userId === writerId;
         });
-      })
-      .catch((error) => {
-        dispatch({ type: ACTION_TYPES.error });
-        console.log(error);
-      });
-    setRefreshComments(false);
-  }, [postId]);
 
-  useEffect(() => {
-    if (isMounted.current) isMounted.current = false;
-    else getComments();
-  }, [getComments, refreshComments]);
+        if (likeControl != null) {
+            setLikeId(likeControl.id);
+            setLiked(true);
+        } else {
+            setLiked(false);
+        }
+    };
+    useEffect(() => {
+        likedOrNot();
+    }, []);
 
-  if (state.error) {
-    return <div>ERROR</div>;
-  } else if (!state.loading) {
-    return <div>Loading...</div>;
-  } else
-    return (
-      <Card
-        sx={{
-          marginBottom: "1rem",
-          borderRadius: 3,
-        }}
-      >
-        <CardHeader
-          avatar={
-            <Link
-              className="avatar-link"
-              to={{ pathname: "/writers/" + writerId }}
+    const handleFavorite = () => {
+        setLiked(!liked);
+        if (liked) {
+            setLikeCount(likeCount - 1);
+            deleteLike();
+        } else {
+            setLikeCount(likeCount + 1);
+            sendLike();
+        }
+    };
+    const setCommentRefresh = () => {
+        setRefreshComments(true);
+    };
+    let axiosConfig = {
+        headers: {
+            Authorization:
+                "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjYyNzI0MzMzLCJleHAiOjE2NjMwNjY2NzV9.fk21fVMuydrPOgD1wdLtxV8DNARR6FsrK5_rVaacMYzr_VpA8EkjUfe4U292e3kHzX1LOVBJBZgGLOUc_DEgaQ",
+        },
+    };
+    const deleteLike = () => {
+        axios
+            .delete("/postlikes/" + likeId, axiosConfig)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    const sendLike = () => {
+        axios
+            .post(
+                "/postlikes",
+                {
+                    postId: postId,
+                    writerId: writerId,
+                },
+                axiosConfig
+            )
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    const getComments = useCallback(() => {
+        const url = "http://localhost:8080";
+        axios
+            .get(url + "/comments?postId=" + postId, axiosConfig)
+            .then((response) => {
+                dispatch({
+                    type: ACTION_TYPES.success,
+                    payload: response.data,
+                });
+            })
+            .catch((error) => {
+                dispatch({ type: ACTION_TYPES.error });
+                console.log(error);
+            });
+        setRefreshComments(false);
+    }, [postId]);
+
+    useEffect(() => {
+        if (isMounted.current) isMounted.current = false;
+        else getComments();
+    }, [getComments, refreshComments]);
+
+    if (state.error) {
+        return <div>ERROR</div>;
+    } else if (!state.loading) {
+        return <div>Loading...</div>;
+    } else
+        return (
+            <Card
+                sx={{
+                    marginBottom: "1rem",
+                    borderRadius: 3,
+                }}
             >
-              <Avatar
-                className="avatar-mui"
-                sx={{ bgcolor: blueGrey[500] }}
-                aria-label="recipe"
-              >
-                {name != null && name.charAt(0).toUpperCase()}
-              </Avatar>
-            </Link>
-          }
-          title={name + " " + lastName}
-          subheader={title}
-        />
-        <CardContent className="alter-border">
-          <Typography
-            multiline
-            noWrap={false}
-            align="left"
-            variant="body1"
-            color="text.secondary"
-          >
-            {content}
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <div className="d-flex justify-content-end">
-            <IconButton aria-label="add to favorites">
-              <FavoriteIcon
-                onClick={handleFavorite}
-                style={liked ? { color: "red" } : null}
-              />
-            </IconButton>
-            <div style={{ fontSize: "1em" }} className="align-self-center ">
-              {likeCount}
-            </div>
-          </div>
+                <CardHeader
+                    avatar={
+                        <Link
+                            className="avatar-link"
+                            to={{ pathname: "/writers/" + writerId }}
+                        >
+                            <Avatar
+                                className="avatar-mui"
+                                sx={{ bgcolor: blueGrey[500] }}
+                                aria-label="recipe"
+                            >
+                                {userName != null &&
+                                    userName.charAt(0).toUpperCase()}
+                            </Avatar>
+                        </Link>
+                    }
+                    title={userName}
+                    subheader={title}
+                />
+                <CardContent className="alter-border">
+                    <Typography
+                        multiline
+                        noWrap={false}
+                        align="left"
+                        variant="body1"
+                        color="text.secondary"
+                    >
+                        {content}
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <div className="d-flex justify-content-end">
+                        <IconButton aria-label="add to favorites">
+                            <FavoriteIcon
+                                onClick={handleFavorite}
+                                style={liked ? { color: "red" } : null}
+                            />
+                        </IconButton>
+                        <div
+                            style={{ fontSize: "1em" }}
+                            className="align-self-center "
+                        >
+                            {likeCount}
+                        </div>
+                    </div>
 
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <CommentIcon></CommentIcon>
-          </ExpandMore>
-        </CardActions>
+                    <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <CommentIcon></CommentIcon>
+                    </ExpandMore>
+                </CardActions>
 
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          {}
-          <CommentForm
-            setCommentRefresh={setCommentRefresh}
-            name={"Görkem Karamolla"}
-            postId={postId}
-            writerId={1}
-          ></CommentForm>
-          <p> All comments for this post</p>
-          {state.loading
-            ? state.comments.map((comment) => (
-                <div className="row d-flex justify-content-center">
-                  <Comment
-                    comment={comment.comment}
-                    postId={comment.postId}
-                    writerId={1}
-                    name={"Görkem Karamolla"}
-                  ></Comment>
-                </div>
-              ))
-            : "Loading"}
-        </Collapse>
-      </Card>
-    );
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    {}
+                    <CommentForm
+                        setCommentRefresh={setCommentRefresh}
+                        name={"Görkem Karamolla"}
+                        postId={postId}
+                        writerId={1}
+                    ></CommentForm>
+                    <p> All comments for this post</p>
+                    {state.loading
+                        ? state.comments.map((comment) => (
+                              <div className="row d-flex justify-content-center">
+                                  <Comment
+                                      comment={comment.comment}
+                                      postId={comment.postId}
+                                      writerId={1}
+                                      name={"Görkem Karamolla"}
+                                  ></Comment>
+                              </div>
+                          ))
+                        : "Loading"}
+                </Collapse>
+            </Card>
+        );
 }
