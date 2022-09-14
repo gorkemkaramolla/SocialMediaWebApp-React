c76434516b65e1bde35815197b78820dab956cd9
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-14 22:49:41 +0300

    reducer UserName

--- src/components/Auth/loginReducer/LoginReducers.js
+++ src/components/Auth/loginReducer/LoginReducers.js
@@ -2,6 +2,7 @@ export const INITIAL_STATE = {
     writerId: 0,
     message: "",
     accessToken: "",
+    error: false,
 };
 export const loginReducer = (state, action) => {
     switch (action.type) {
@@ -10,10 +11,12 @@ export const loginReducer = (state, action) => {
                 writerId: action.payload.writerId,
                 message: action.payload.message,
                 accessToken: action.payload.accessToken,
+                userName: action.payload.userName,
             };
         case "FETCH_ERROR":
             return {
                 ...state,
+                error: true,
             };
         default:
             return state;
