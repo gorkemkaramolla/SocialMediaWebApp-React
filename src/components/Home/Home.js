8e30b329652b0af9d469c61d9d45dfdd75326959
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-27 14:49:51 +0300

    profile image home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,83 +10,91 @@ import { ACTION_TYPES } from "./homeReducers/homeActionTypes";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-    const [state, dispatch] = useReducer(homeReducer, INITIAL_STATE);
-    let history = useNavigate();
-    let axiosConfig = {
-        headers: {
-            Authorization: localStorage.getItem("access"),
-        },
-    };
-    const refreshPost = async () => {
-        await axios
-            .get("/posts", axiosConfig)
-            .then((response) => {
-                console.log(response);
-                setTimeout(() => {
-                    dispatch({
-                        type: ACTION_TYPES.success,
-                        payload: response.data,
-                    });
-                }, 1000);
-            })
-            .catch((error) => {
-                dispatch({ type: ACTION_TYPES.error });
-
-                console.log(error);
-                localStorage.clear();
-            });
-    };
-
-    useEffect(() => {
-        refreshPost();
-    }, []);
-
-    if (state.error) {
+  const [state, dispatch] = useReducer(homeReducer, INITIAL_STATE);
+  let history = useNavigate();
+  let axiosConfig = {
+    headers: {
+      Authorization: localStorage.getItem("access"),
+    },
+  };
+  const refreshPost = async () => {
+    await axios
+      .get("/posts", axiosConfig)
+      .then((response) => {
+        console.log(response.data);
         setTimeout(() => {
-            history("/", { replace: true });
-        }, 300);
-    } else if (!state.loading) {
-        return <Loading />;
-    } else {
-        return (
-            <div className="container">
-                <div
-                    className="row d-flex justify-content-center home-bg"
-                    style={{
-                        minWidth: "360px",
-                        padding: "30px",
-                        borderRadius: "20px",
-                    }}
-                >
-                    <div className="d-flex flex-column justify-content-center ">
-                        <div
-                            style={{ minWidth: "350px" }}
-                            className="col-lg-6 col-md-6 col-xs-6 col-sm-6 col-xxl-6 col-6 col-xs-fluid align-self-center"
-                        >
-                            <PostForm
-                                userName={localStorage.getItem("userName")}
-                                writerId={localStorage.getItem("user")}
-                                refreshPost={refreshPost}
-                            ></PostForm>
-                        </div>
-                        {state.postList.map((element) => (
-                            <div
-                                style={{ minWidth: "350px" }}
-                                className="col-lg-6 col-md-6 col-xs-6 col-sm-6 col-xxl-6 col-6 align-self-center  "
-                            >
-                                <PostCard
-                                    postId={element.id}
-                                    writerId={localStorage.getItem("user")}
-                                    userName={element.userName}
-                                    title={element.title}
-                                    content={element.content}
-                                    postLikes={element.postLikes}
-                                ></PostCard>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+          dispatch({
+            type: ACTION_TYPES.success,
+            payload: response.data,
+          });
+        }, 1000);
+      })
+      .catch((error) => {
+        dispatch({ type: ACTION_TYPES.error });
+
+        console.log(error);
+        localStorage.clear();
+      });
+  };
+
+  useEffect(() => {
+    refreshPost();
+  }, []);
+
+  if (state.error) {
+    setTimeout(() => {
+      history("/", { replace: true });
+    }, 300);
+  } else if (!state.loading) {
+    return <Loading />;
+  } else {
+    return (
+      <div className="container">
+        <div
+          className="row d-flex justify-content-center home-bg"
+          style={{
+            minWidth: "360px",
+            padding: "30px",
+            borderRadius: "20px",
+          }}
+        >
+          <div className="d-flex flex-column justify-content-center ">
+            <div
+              style={{ minWidth: "350px" }}
+              className="col-lg-6 col-md-6 col-xs-6 col-sm-6 col-xxl-6 col-6 col-xs-fluid align-self-center"
+            >
+              <PostForm
+                userName={localStorage.getItem("userName")}
+                writerId={localStorage.getItem("user")}
+                refreshPost={refreshPost}
+                profileImagePath={
+                  localStorage.getItem("profileImagePath") === "" ||
+                  localStorage.getItem("profileImagePath") == null
+                    ? "user.png"
+                    : localStorage.getItem("profileImagePath")
+                }
+              ></PostForm>
             </div>
-        );
-    }
+            {state.postList.map((element) => (
+              <div
+                key={element.id}
+                style={{ minWidth: "350px" }}
+                className="col-lg-6 col-md-6 col-xs-6 col-sm-6 col-xxl-6 col-6 align-self-center  "
+              >
+                <PostCard
+                  postId={element.id}
+                  writerId={localStorage.getItem("user")}
+                  userName={element.userName}
+                  title={element.title}
+                  content={element.content}
+                  postLikes={element.postLikes}
+                  profileImagePath={element.imagePath}
+                ></PostCard>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
