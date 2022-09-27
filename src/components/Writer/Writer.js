8f1ae500f2cdb5c748e66fe3958f439300c2caa6
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-27 14:51:32 +0300

    Writer Get Request Updated

--- src/components/Writer/Writer.js
+++ src/components/Writer/Writer.js
@@ -3,54 +3,66 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import { FormHelperText, TextField } from "@mui/material";
 import Button from "@mui/material/Button";
-import { InputAdornment } from "@mui/material";
 import PostForm from "../PostComponents/PostForm";
 import ProfileCard from "./ProfileCard";
 import "./writer.scss";
 export default function Writer() {
-    const [writerInfo, setWriterInfo] = useState({
-        userName: "",
-        bio: "",
-        email: "",
-    });
-    let axiosConfig = {
-        headers: {
-            Authorization: localStorage.getItem("access"),
-        },
-    };
-    useEffect(() => {
-        getWriterInformations();
-    }, []);
-    const getWriterInformations = () => {
-        axios
-            .get("/writers/" + writerId, axiosConfig)
-            .then((response) => {
-                console.log(response.data);
-                setWriterInfo((prev) => ({
-                    ...prev,
-                    userName: response.data.userName,
+  const [writerInfo, setWriterInfo] = useState({
+    userName: "",
+    bio: "",
+    email: "",
+    imgPath: null,
+  });
+  let axiosConfig = {
+    headers: {
+      Authorization: localStorage.getItem("access"),
+    },
+  };
+  useEffect(() => {
+    getWriterInformations();
+  });
+  const getWriterInformations = () => {
+    axios
+      .get("/writers/" + writerId, axiosConfig)
+      .then((response) => {
+        console.log(response.data);
+        setWriterInfo((prev) => ({
+          ...prev,
+          userName: response.data.userName,
+          bio: response.data.bio,
+          email: response.data.email,
+          imgPath: response.data.imgPath,
+        }));
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+    console.log(writerInfo);
+  };
 
-                    bio: response.data.bio,
-                    email: response.data.email,
-                }));
-            })
-            .catch((e) => {
-                console.error(e);
-            });
-    };
-
-    const { writerId } = useParams();
-    return (
-        <div className="container ">
-            <div className="row d-flex justify-content-start">
-                <div className="col-lg-12 col-md-fluid col-xs-fluid col-sm-fluid col-xxl-fluid col-fluid col-xs-fluid d-flex justify-content-center ">
-                    <ProfileCard
-                        userName={localStorage.getItem("userName")}
-                        bio={writerInfo.bio}
-                    ></ProfileCard>
-                </div>
-            </div>
-            <div className="row d-flex justify-content-center"></div>
+  const { writerId } = useParams();
+  return (
+    <div className="container ">
+      <div className="row d-flex justify-content-start">
+        <div className="col-lg-12 col-md-fluid col-xs-fluid col-sm-fluid col-xxl-fluid col-fluid col-xs-fluid d-flex justify-content-center ">
+          <ProfileCard
+            image={writerInfo.imgPath}
+            userName={writerInfo.userName}
+            bio={writerInfo.bio}
+            imgPath={writerInfo.imgPath}
+            requestedWriter={writerId}
+          />
         </div>
-    );
+        <div className="col-lg-12 col-md-fluid col-xs-fluid col-sm-fluid col-xxl-fluid col-fluid col-xs-fluid d-flex justify-content-center ">
+          {writerId === localStorage.getItem("user") ? (
+            <PostForm
+              userName={localStorage.getItem("userName")}
+              writerId={writerId}
+            ></PostForm>
+          ) : null}
+        </div>
+      </div>
+      <div className="row d-flex justify-content-center"></div>
+    </div>
+  );
 }
