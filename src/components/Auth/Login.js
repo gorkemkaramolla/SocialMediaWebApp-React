db2eecc837836e7775fbd52501fbfd64fe46fa91
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-29 13:25:58 +0300

    useFormik

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -11,26 +11,21 @@ import { ACTION_TYPES } from "./loginReducer/loginActionTypes";
 import RegisterAlert from "../Errors/RegisterAlert";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import { useFormik } from "formik";
+import * as yup from "yup";
 export default function Login() {
   let history = useNavigate();
-  const [credentials, setCredentials] = useState({ email: "", password: "" });
   const [IsPasswordVisible, setIspasswordVisible] = useState(false);
   const [state, dispatch] = useReducer(loginReducer, INITIAL_STATE);
   const handlePasswordEye = () => {
     setIspasswordVisible(!IsPasswordVisible);
   };
-  const handleInputs = (event) => {
-    setCredentials((prev) => ({
-      ...prev,
-      [event.target.name]: event.target.value,
-    }));
-  };
 
-  const submitForm = () => {
+  const submitForm = (email, password) => {
     axios
       .post("/auth/login", {
-        email: credentials.email,
-        password: credentials.password,
+        email: email,
+        password: password,
       })
       .then((response) => {
         dispatch({
@@ -58,111 +53,137 @@ export default function Login() {
     },
   });
 
-  const eyeIconCss = {
-    color: "red",
-  };
-  return (
-    <div
-      style={{ minHeight: "calc(100vh - 80px)" }}
-      className="d-flex flex-column  justify-content-center align-items-center"
-    >
-      <div className="" style={{ fontFamily: "didot", fontStyle: "italic" }}>
-        <h1>Login</h1>
-      </div>
-      <div
-        className="container"
-        style={{
-          padding: "4em 6em",
+  const validationSchema = yup.object({
+    email: yup
+      .string("Enter your email")
+      .email("Enter a valid email")
+      .required("Email is required"),
+    password: yup
+      .string("Enter your password")
 
-          maxWidth: "800px",
-        }}
+      .required("Password is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      submitForm(values.email, values.password);
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div
+        style={{ minHeight: "calc(100vh - 80px)" }}
+        className="d-flex flex-column  justify-content-center align-items-center"
       >
-        <div className="row">
-          <div className=" ">
-            <div className="d-flex flex-column   ">
-              <TextField
-                sx={{ background: "rgba(236,239,242,255)" }}
-                value={credentials.email}
-                name="email"
-                onChange={handleInputs}
-                className="mb-3 "
-                label="Email"
-              ></TextField>
-              <TextField
-                sx={{ background: "rgba(236,239,242,255)" }}
-                value={credentials.password}
-                name="password"
-                onChange={handleInputs}
-                className="mb-3 "
-                label="Password"
-                type={IsPasswordVisible ? null : "password"}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment
-                      onClick={handlePasswordEye}
-                      position="end"
-                      component="div"
-                      style={{ paddingLeft: "-14px", cursor: "pointer" }}
-                    >
-                      {IsPasswordVisible ? (
-                        <FaEyeSlash style={{ color: "#4169E1 " }} />
-                      ) : (
-                        <FaEye />
-                      )}
-                    </InputAdornment>
-                  ),
-                }}
-              ></TextField>
-            </div>
-            <div className=" align-self-center ">
-              <div className="d-flex flex-column  ">
-                <ThemeProvider theme={theme}>
-                  <LoadingButton
-                    sx={{
-                      minHeight: 56,
-                    }}
-                    loadingPosition="end"
-                    endIcon={<SendIcon />}
-                    variant="contained"
-                    color="primary"
-                    onClick={submitForm}
-                  >
-                    Submit
-                  </LoadingButton>
-                  {state.error === true ? (
-                    <RegisterAlert
-                      infoMessage="Your Email or Password is Wrong"
-                      header="Authorization Failed"
-                      type="danger"
-                    ></RegisterAlert>
-                  ) : null}
-                  <div className="d-flex justify-content-start">
-                    <FormHelperText
-                      sx={{
-                        backgroundColor: "rgba(237,239,241,255)",
-                        fontSize: "16px",
-                        color: "black",
-                        paddingTop: "20px",
-                      }}
-                    >
-                      Don't you have an account?
-                      <Link
-                        style={{
-                          color: "#0095f6",
-                        }}
-                        className="nav-link"
-                        to="/register"
+        <div className="" style={{ fontFamily: "didot", fontStyle: "italic" }}>
+          <h1>Login</h1>
+        </div>
+        <div
+          className="container"
+          style={{
+            padding: "4em 6em",
+
+            maxWidth: "800px",
+          }}
+        >
+          <div className="row">
+            <div className=" ">
+              <div className="d-flex flex-column   ">
+                <TextField
+                  sx={{ background: "rgba(236,239,242,255)" }}
+                  value={formik.values.email}
+                  name="email"
+                  onChange={formik.handleChange}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={formik.touched.email && formik.errors.email}
+                  className="mb-3 "
+                  label="Email"
+                ></TextField>
+                <TextField
+                  sx={{ background: "rgba(236,239,242,255)" }}
+                  name="password"
+                  className="mb-3 "
+                  label="Password"
+                  type={IsPasswordVisible ? null : "password"}
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                  helperText={formik.touched.password && formik.errors.password}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment
+                        onClick={handlePasswordEye}
+                        position="end"
+                        component="div"
+                        style={{ paddingLeft: "-14px", cursor: "pointer" }}
                       >
-                        Sign up
-                      </Link>
-                    </FormHelperText>
-                  </div>
-                </ThemeProvider>
+                        {IsPasswordVisible ? (
+                          <FaEyeSlash style={{ color: "#4169E1 " }} />
+                        ) : (
+                          <FaEye />
+                        )}
+                      </InputAdornment>
+                    ),
+                  }}
+                ></TextField>
+              </div>
+              <div className=" align-self-center ">
+                <div className="d-flex flex-column  ">
+                  <ThemeProvider theme={theme}>
+                    <LoadingButton
+                      type="submit"
+                      sx={{
+                        minHeight: 56,
+                      }}
+                      loadingPosition="end"
+                      endIcon={<SendIcon />}
+                      variant="contained"
+                      color="primary"
+                    >
+                      Submit
+                    </LoadingButton>
+                    {state.error === true ? (
+                      <RegisterAlert
+                        infoMessage="Your Email or Password is Wrong"
+                        header="Authorization Failed"
+                        type="danger"
+                      ></RegisterAlert>
+                    ) : null}
+                    <div className="d-flex justify-content-start">
+                      <FormHelperText
+                        sx={{
+                          backgroundColor: "rgba(237,239,241,255)",
+                          fontSize: "16px",
+                          color: "black",
+                          paddingTop: "20px",
+                        }}
+                      >
+                        Don't you have an account?
+                        <Link
+                          style={{
+                            color: "#0095f6",
+                          }}
+                          className="nav-link"
+                          to="/auth/register"
+                        >
+                          Sign up
+                        </Link>
+                      </FormHelperText>
+                    </div>
+                  </ThemeProvider>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
