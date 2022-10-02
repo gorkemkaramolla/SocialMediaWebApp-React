e349bf44028a99225438d7693a5d4e0f03e57165
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-10-02 23:06:31 +0300

    Routes and Layouts

--- src/App.js
+++ src/App.js
@@ -2,47 +2,58 @@ import React from "react";
 import "./App.scss";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home/Home";
-import Register from "./components/Auth/Register";
-import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register/Register";
+import Login from "./components/Auth/Login/Login";
 import Writer from "./components/Writer/Writer";
 import NavBarComponent from "./components/Navbar/NavBarComponent";
 import { useEffect, useState } from "react";
 import { HomeLayout } from "./components/Layouts/HomeLayout";
+import Page404 from "./components/Errors/ErrorPages/404/Page404";
 import AuthLayout from "./components/Layouts/AuthLayout";
+import { ThemeContextProvider } from "./components/contexts/ThemeContext";
+import Settings from "./components/Settings/Settings";
 function App() {
-  const [userData, setuserData] = useState("");
+    const [userData, setuserData] = useState("");
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route>
-          <Route
-            path="/"
-            element={
-              <div>
-                <HomeLayout />
-              </div>
-            }
-          >
-            <Route index path="/home" element={<Home />}></Route>
-            <Route
-              exact
-              path="/profile/:writerId"
-              element={
-                <div>
-                  <Writer />
-                </div>
-              }
-            ></Route>
-          </Route>
-        </Route>
-        <Route path="/auth" element={<AuthLayout />}>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="register" element={<Register />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <ThemeContextProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route>
+                        <Route
+                            path="/"
+                            element={
+                                <div>
+                                    <HomeLayout />
+                                </div>
+                            }
+                        >
+                            <Route
+                                index
+                                path="/home"
+                                element={<Home />}
+                            ></Route>
+                            <Route
+                                exact
+                                path="/profile/:writerId"
+                                element={
+                                    <div>
+                                        <Writer />
+                                    </div>
+                                }
+                            ></Route>
+                        </Route>
+                    </Route>
+                    <Route path="/auth" element={<AuthLayout />}>
+                        <Route path="login" element={<Login />}></Route>
+                        <Route path="register" element={<Register />}></Route>
+                    </Route>
+                    <Route path="*" element={<Page404 />}></Route>
+                </Routes>
+                <Settings></Settings>
+            </BrowserRouter>
+        </ThemeContextProvider>
+    );
 }
 
 export default App;
